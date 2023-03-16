70fb422c8ea612443d0e46eb7459cc7b6f088edb
hax4dazy <Uitvinder.gdm@gmail.com>
2023-03-16 12:46:01 +0100

    Adding navbar and card into own files

---
+++ src/components/card.svelte
@@ -0,0 +1,22 @@
+<style>
+glass {
+    /* From https://css.glass */
+  background: rgba(179, 161, 161, 0.55);
+  border-radius: 16px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(14.7px);
+  -webkit-backdrop-filter: blur(14.7px);
+  border: 1px solid rgba(179, 161, 161, 0.3);
+}
+</style>
+
+<div class="card card-compact w-96 bg-base-100 shadow-xl glass">
+  <figure><img src="" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">DeepSea</h2>
+    <p>This is an example card for the DeepSea project</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Check it out!</button>
+    </div>
+  </div>
+</div>
---
+++ src/components/navbar.svelte
@@ -0,0 +1,11 @@
+<div class="navbar bg-base-100 h-14">
+  <div class="flex-1">
+    <a class="btn btn-ghost normal-case text-xl glass">Gideon De Marcas</a>
+  </div>
+  <div class="flex-none">
+    <ul class="menu menu-horizontal px-1">
+      <li><a>Item 1</a></li>
+      <li><a>Item 2</a></li>
+    </ul>
+  </div>
+</div>
