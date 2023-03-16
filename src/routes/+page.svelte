8d5d96823ed1699dfbc57d3ccc85574b4701f5b0
hax4dazy <Uitvinder.gdm@gmail.com>
2023-03-16 12:48:24 +0100

    Removing stupid facebook footer icon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,15 @@
 <script>
-import Footer from '../components/footer.svelte'
-import Card from '../components/card.svelte'
-import Navbar from '../components/navbar.svelte'
+  import Footer from '../components/footer.svelte'
+  import Card from '../components/card.svelte'
+  import Navbar from '../components/navbar.svelte'
 </script>
 
 <html data-theme="light"></html>
 <div class='bg-gradient-to-r from-purple-500 to-pink-500 absolute h-screen w-screen -z-10'>
-<Navbar/>
-<div class="grid grid-cols-4 gap-4 h-14 mt-3">
-  <Card/>
+  <Navbar/>
+  <div class="grid grid-cols-4 gap-4 h-14 mt-3">
+    <Card/>
+  </div>
 </div>
-</div>
+
 <Footer/>
