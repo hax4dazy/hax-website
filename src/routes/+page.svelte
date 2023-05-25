18aa451e8c9eaa5fad5cd419578b922cffa66a93
hax4dazy <16256978+hax4dazy@users.noreply.github.com>
2023-05-25 10:27:31 +0200

    Add proper logo size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,34 +4,35 @@
   import Navbar from '../components/navbar.svelte'
   import CardNoButton from '../components/card-no-button.svelte'
   let imnumber = Math.floor(Math.random() * 8) + 1;
-  let ramble = "I'm a software developer, and I'm passionate about building things that make a difference. I'm currently working on a few projects, and I'm always looking for new opportunities. If you have any questions, feel free to reach out to me!"
+  let ramble = "Hello! I'm a software developer, and I'm passionate about building things that make a difference. I'm currently working on a few projects, and I'm always looking for new opportunities. If you have any questions, feel free to reach out to me!"
   let review = "Gideon is a good developer and I like him 👍"
 </script>
 
+
 <div class='bg-gradient-to-r from-purple-500 to-pink-500 absolute'>
   <Navbar/>
-  <div class="grid grid-cols-12 gap-4 mt-5 ml-3 mr-3">
+  <div class="grid md:grid-cols-12 gap-4 mt-5 ml-3 mr-3">
       <div class="col-span-8">
         <div class="grid grid-cols-1">
           <div class="card card-compact bg-base-100 shadow-xl">
-            <figure><img width="500" height="200" src="../src/assets/gideon.pink.png" alt="Its me!" /></figure>
+            <figure><img width="100%" height="100%" src="../src/assets/gideon.pink.png" alt="Its me!" /></figure>
             <div class="card-body">
-              <h2 class="card-title">Welcome to my portfolio!</h2>
+              <h2 class="card-title">Expect the unexpected</h2>
               <p>{ramble}</p>
             </div>
           </div>
         </div>
       </div>
-    <div class="col-span-4 sm:shrink-0">
+    <div class="col-span-4">
       <div class="avatar">
         <div class="w-autorounded">
-          <img class="rounded-box bg-base-100 shadow-xl" src="../src/assets/{imnumber}.jpeg" alt="hey its me!"/>
+          <img class="rounded-box bg-base-100 shadow-xl" src="../src/assets/{imnumber}.jpeg" alt="hey its me!" object-position="center"/>
         </div>
       </div>
     </div>
   </div>
 
-  <div class="grid lg:grid-cols-12 md:grid-cols-2 gap-4 ml-3 mr-3 mt-3">
+  <div class="grid xl:grid-cols-20 lg:grid-cols-12 md:grid-cols-2 gap-4 ml-3 mr-3 mt-3">
     <div class="lg:col-span-3">
       <CardNoButton title="About Me" img="../src/assets/tmp.jpg" bio="{review}"/>
     </div>
