b4f5a03025c73ddd2e78e8e6e35327f69715f732
hax4dazy <16256978+hax4dazy@users.noreply.github.com>
2023-05-31 23:08:15 +0200

    Add in random pride flag in logo
    this is still broken but planning to fix later ish

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,6 +1,14 @@
+<script>
+  import '@vkea/pridecss/css/pride-lite.css';
+  const LGBTQ = ['pride-bi', 'pride-trans', 'pride-quasar', 'pride-gay'];
+  let randomLGBTQ = LGBTQ[Math.floor(Math.random() * LGBTQ.length)];
+  console.log(randomLGBTQ);
+
+</script>
+
 <div class="navbar bg-base-100">
   <div class="flex-1">
-    <a class="btn btn-ghost normal-case text-xl glass" href="/">Gideon De Marcas</a>
+    <a class="btn btn-ghost normal-case text-xl glass {randomLGBTQ} text-white" href="/">Gideon De Marcas</a>
   </div>
   <div class="flex-none">
     <ul class="menu menu-horizontal px-1">
