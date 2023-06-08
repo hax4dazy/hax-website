987c111517e05fcecc3e61a4b1ff1f367b2d0651
hax4dazy <16256978+hax4dazy@users.noreply.github.com>
2023-06-08 23:25:50 +0200

    change name of variable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -74,10 +74,10 @@
 
   <div class="grid grid-cols-12 gap-4 mx-auto mt-5">
     <div class="col-span-12">
-      <Card title="DeepSea" img={deepsealogo} link="https://github.com/Team-Neptune/DeepSea" bio="DeepSea is an AiO (All In One) Custom Firmware package for the Nintendo Switch. It is used by thousands of people and maintained by me!"/>
+      <Card title="DeepSea" img={deepsealogo} href="https://github.com/Team-Neptune/DeepSea" bio="DeepSea is an AiO (All In One) Custom Firmware package for the Nintendo Switch. It is used by thousands of people and maintained by me!"/>
     </div>
     <div class="col-span-12">
-      <Card title="Gideon.pink" img="{site}" link="https://git.gideon.pink/Hax4dayz/hax-website" bio="This website was fully coded (with some help from other sources) by me!"/>
+      <Card title="Gideon.pink" img="{site}" href="https://git.gideon.pink/Hax4dayz/hax-website" bio="This website was fully coded (with some help from other sources) by me!"/>
     </div>
   </div>
 </div>
