62b8f22175c2777944abd1ea6ca308c7d272f0e9
hax4dazy <16256978+hax4dazy@users.noreply.github.com>
2023-06-08 14:27:34 +0200

    Random commit to test building

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,6 +21,7 @@
   //const
   const images = [pfp1, pfp2, pfp3, pfp4, pfp5, pfp6, pfp7, pfp8, pfp9];
   //variables
+  let egg = "egg"
   let randomIndex = Math.floor(Math.random() * images.length);
   let pfp = images[randomIndex];
   let ramble =
