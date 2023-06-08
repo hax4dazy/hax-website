0bd3f4a186331b125d83f54d300fdbdd86c86d69
hax4dazy <16256978+hax4dazy@users.noreply.github.com>
2023-06-08 13:14:23 +0200

    Add in cursed image loading and fix some other things

--- src/components/card-no-button.svelte
+++ src/components/card-no-button.svelte
@@ -1,15 +1,17 @@
 <script>
-export let title;
-export let bio;
-export let img;
+  export let title;
+  export let bio;
+  export let img;
+  import tmp from "../assets/tmp.jpg";
 </script>
 
-<div class="card card-compact bg-base-100 shadow-xl">
-  <figure><img src="{img}" alt="{title}" /></figure>
+<div class="card card-compact bg-base-100 shadow-xl w-fit">
+  <figure class="max-w-sm">
+    <img src={img} alt={title} />
+  </figure>
   <div class="card-body">
     <h2 class="card-title">{title}</h2>
     <p>{bio}</p>
-    <div class="card-actions justify-end">
-    </div>
+    <div class="card-actions justify-end" />
   </div>
 </div>
